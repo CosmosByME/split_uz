7d1902ee390398491df3f048c4dd8cc64f32f1f5
Admin <egamovmuhammad.sr@gmail.com>
2026-06-17 12:49:23 +0500

    Great Purge💀

--- android/app/src/main/kotlin/com/example/split_uz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.split_uz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/data/repository/user_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:split_uz/data/model/user.dart';
-
-abstract class UserRepository {
-  Future<UserModel?> getUser(String uid);
-  Future<void> setUser(UserModel user);
-} 
